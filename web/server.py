055c8833b875df8f08599de40a3c79af49e1c1f8
Kiril Kafadarov <kkafadarov@gmail.com>
2018-11-09 16:32:31 -0800

    Add stub for text generation API

--- web/server.py
+++ web/server.py
@@ -7,3 +7,8 @@ app = Flask(__name__)
 @app.route("/")
 def index_page():
     return render_template('index.html')
+
+@app.route('/generate/<string:text_type>')
+def generate_text(text_type):
+    # show the post with the given id, the id is an integer
+    return 'Hi - this is a sample but you asked for text type %s' % text_type
